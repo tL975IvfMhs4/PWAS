9ba4dda8889306a383e80043b0eeefb6eb9c4583
tL975IvfMhs4 <jean.dupont200@hotmail.com>
2026-08-06 23:05:40 +0200

    démarrage projet

--- pwas-launcher/src/main/java/com/github/tL975IvfMhs4/App.java
+++ pwas-launcher/src/main/java/com/github/tL975IvfMhs4/App.java
@@ -1,10 +1,12 @@
 package com.github.tL975IvfMhs4;
 
+import com.github.tL975IvfMhs4.serveur.Startup;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Startup.run();
     }
 }
